39fc0f306b90a9d508ffc89772af25d082d6c3da
advyunjin <yunjin.jiang@advantech.com.cn>
2021-01-26 10:01:03 +0800

    update to rk3288_linux_release_v2.3.0_20201203

--- rockchip/common/mkfirmware.sh
+++ rockchip/common/mkfirmware.sh
@@ -6,19 +6,43 @@ SCRIPT_DIR=$(dirname $(realpath $BASH_SOURCE))
 TOP_DIR=$(realpath $SCRIPT_DIR/../../..)
 cd $TOP_DIR
 
+function unset_board_config_all()
+{
+	local tmp_file=`mktemp`
+	grep -o "^export.*RK_.*=" `find $TOP_DIR/device/rockchip -name "Board*.mk" -type f` -h | sort | uniq > $tmp_file
+	source $tmp_file
+	rm -f $tmp_file
+}
+unset_board_config_all
+
 source $TOP_DIR/device/rockchip/.BoardConfig.mk
 ROCKDEV=$TOP_DIR/rockdev
 PARAMETER=$TOP_DIR/device/rockchip/$RK_TARGET_PRODUCT/$RK_PARAMETER
-OEM_DIR=$TOP_DIR/device/rockchip/oem/$RK_OEM_DIR
+if [ "${RK_OEM_DIR}x" != "x" ];then
+	OEM_DIR=$TOP_DIR/device/rockchip/oem/$RK_OEM_DIR
+else
+	OEM_DIR=
+fi
 USER_DATA_DIR=$TOP_DIR/device/rockchip/userdata/$RK_USERDATA_DIR
 MISC_IMG=$TOP_DIR/device/rockchip/rockimg/$RK_MISC
 ROOTFS_IMG=$TOP_DIR/$RK_ROOTFS_IMG
+ROOTFS_IMG_SOURCE=$TOP_DIR/buildroot/output/$RK_CFG_BUILDROOT/images/rootfs.$RK_ROOTFS_TYPE
 RAMBOOT_IMG=$TOP_DIR/buildroot/output/$RK_CFG_RAMBOOT/images/ramboot.img
 RECOVERY_IMG=$TOP_DIR/buildroot/output/$RK_CFG_RECOVERY/images/recovery.img
+if which fakeroot; then
+FAKEROOT_TOOL="`which fakeroot`"
+else
+	echo -e "Install fakeroot First."
+	echo -e "  sudo apt-get install fakeroot"
+	exit -1
+fi
+OEM_FAKEROOT_SCRIPT=$ROCKDEV/oem.fs
+USERDATA_FAKEROOT_SCRIPT=$ROCKDEV/userdata.fs
 TRUST_IMG=$TOP_DIR/u-boot/trust.img
 UBOOT_IMG=$TOP_DIR/u-boot/uboot.img
 BOOT_IMG=$TOP_DIR/kernel/$RK_BOOT_IMG
 LOADER=$TOP_DIR/u-boot/*_loader_v*.bin
+SPL=$TOP_DIR/u-boot/*_loader_spl.bin
 #SPINOR_LOADER=$TOP_DIR/u-boot/*_loader_spinor_v*.bin
 MKIMAGE=$SCRIPT_DIR/mk-image.sh
 mkdir -p $ROCKDEV
@@ -26,7 +50,12 @@ mkdir -p $ROCKDEV
 # Require buildroot host tools to do image packing.
 if [ ! -d "$TARGET_OUTPUT_DIR" ]; then
     echo "Source buildroot/build/envsetup.sh"
-    source $TOP_DIR/buildroot/build/envsetup.sh $RK_CFG_BUILDROOT
+	if [ "${RK_CFG_RAMBOOT}x" != "x" ];then
+		source $TOP_DIR/buildroot/build/envsetup.sh $RK_CFG_RAMBOOT
+	fi
+	if [ "${RK_CFG_BUILDROOT}x" != "x" ];then
+		source $TOP_DIR/buildroot/build/envsetup.sh $RK_CFG_BUILDROOT
+	fi
 fi
 
 check_partition_size() {
@@ -72,17 +101,23 @@ check_partition_size() {
 				fi
 			;;
 			recovery)
-				if [ $part_size_bytes -lt `du -b $RECOVERY_IMG | awk '{print $1}'` ]
+				if [ -f $RECOVERY_IMG ]
 				then
-					echo -e "\e[31m error: recovery image size exceed parameter! \e[0m"
-					return -1
+					if [ $part_size_bytes -lt `du -b $RECOVERY_IMG | awk '{print $1}'` ]
+					then
+						echo -e "\e[31m error: recovery image size exceed parameter! \e[0m"
+						return -1
+					fi
 				fi
 			;;
 			rootfs)
-				if [ $part_size_bytes -lt `du -bD $ROOTFS_IMG | awk '{print $1}'` ]
+				if [ -f $ROOTFS_IMG ]
 				then
-					echo -e "\e[31m error: rootfs image size exceed parameter! \e[0m"
-					return -1
+					if [ $part_size_bytes -lt `du -bD $ROOTFS_IMG | awk '{print $1}'` ]
+					then
+						echo -e "\e[31m error: rootfs image size exceed parameter! \e[0m"
+						return -1
+					fi
 				fi
 			;;
 		esac
@@ -98,6 +133,10 @@ then
 		echo "done."
 	else
 		echo "warning: $ROOTFS_IMG not found!"
+		if [ -f $ROOTFS_IMG_SOURCE ];then
+			echo "Fallback to $ROOTFS_IMG_SOURCE"
+			ln -rsf $ROOTFS_IMG_SOURCE $ROCKDEV/rootfs.img
+		fi
 	fi
 fi
 
@@ -135,21 +174,46 @@ then
 	fi
 fi
 
-if [ $RK_OEM_DIR ]
+if [ "${RK_OEM_BUILDIN_BUILDROOT}x" != "YESx" ]
 then
-	if [ -d $OEM_DIR ]
+	if [ -d "$OEM_DIR" ]
 	then
-		$MKIMAGE $OEM_DIR $ROCKDEV/oem.img $RK_OEM_FS_TYPE
+		echo "#!/bin/sh" > $OEM_FAKEROOT_SCRIPT
+		echo "set -e" >> $OEM_FAKEROOT_SCRIPT
+		if [ -d $OEM_DIR/www ]; then
+			echo "chown -R www-data:www-data $OEM_DIR/www" >> $OEM_FAKEROOT_SCRIPT
+		fi
+		if [ "$RK_OEM_FS_TYPE" = "ubi" ]; then
+			echo "$MKIMAGE $OEM_DIR $ROCKDEV/oem.img $RK_OEM_FS_TYPE $RK_OEM_PARTITION_SIZE oem $RK_UBI_PAGE_SIZE $RK_UBI_BLOCK_SIZE"  >> $OEM_FAKEROOT_SCRIPT
+		else
+			echo "$MKIMAGE $OEM_DIR $ROCKDEV/oem.img $RK_OEM_FS_TYPE"  >> $OEM_FAKEROOT_SCRIPT
+		fi
+		chmod a+x $OEM_FAKEROOT_SCRIPT
+		$FAKEROOT_TOOL -- $OEM_FAKEROOT_SCRIPT
+		rm -f $OEM_FAKEROOT_SCRIPT
 	else
 		echo "warning: $OEM_DIR  not found!"
+	fi
+else
+	if [ -f "$TOP_DIR/buildroot/output/$RK_CFG_BUILDROOT/images/oem.img" ]; then
+		ln -sfr $TOP_DIR/buildroot/output/$RK_CFG_BUILDROOT/images/oem.img $ROCKDEV/oem.img
 	fi
 fi
 
 if [ $RK_USERDATA_DIR ]
 then
-	if [ -d $USER_DATA_DIR ]
+	if [ -d "$USER_DATA_DIR" ]
 	then
-		$MKIMAGE $USER_DATA_DIR $ROCKDEV/userdata.img $RK_USERDATA_FS_TYPE
+		echo "#!/bin/sh" > $USERDATA_FAKEROOT_SCRIPT
+		echo "set -e" >> $USERDATA_FAKEROOT_SCRIPT
+		if [ "$RK_USERDATA_FS_TYPE" = "ubi" ]; then
+			echo "$MKIMAGE $USER_DATA_DIR $ROCKDEV/userdata.img $RK_USERDATA_FS_TYPE $RK_USERDATA_PARTITION_SIZE userdata $RK_UBI_PAGE_SIZE $RK_UBI_BLOCK_SIZE"  >> $USERDATA_FAKEROOT_SCRIPT
+		else
+			echo "$MKIMAGE $USER_DATA_DIR $ROCKDEV/userdata.img $RK_USERDATA_FS_TYPE"  >> $USERDATA_FAKEROOT_SCRIPT
+		fi
+		chmod a+x $USERDATA_FAKEROOT_SCRIPT
+		$FAKEROOT_TOOL -- $USERDATA_FAKEROOT_SCRIPT
+		rm -f $USERDATA_FAKEROOT_SCRIPT
 	else
 		echo "warning: $USER_DATA_DIR not found!"
 	fi
@@ -164,6 +228,10 @@ else
         echo -e "\e[31m error: $UBOOT_IMG not found! \e[0m"
 fi
 
+if [ "$RK_UBOOT_FORMAT_TYPE" = "fit" ]; then
+        rm -f $ROCKDEV/trust.img
+        echo "uboot fotmat type is fit, so ignore trust.img..."
+else
 if [ -f $TRUST_IMG ]
 then
         echo -n "create trust.img..."
@@ -172,15 +240,21 @@ then
 else
         echo -e "\e[31m error: $TRUST_IMG not found! \e[0m"
 fi
+fi
 
 if [ -f $LOADER ]
 then
         echo -n "create loader..."
         ln -rsf $LOADER $ROCKDEV/MiniLoaderAll.bin
         echo "done."
+elif [ -f $SPL ]
+then
+	echo -n "create spl..."
+        ln -rsf $SPL $ROCKDEV/MiniLoaderAll.bin
+        echo "done."
 else
 	echo -e "\e[31m error: $LOADER not found,or there are multiple loaders! \e[0m"
-	rm $LOADER
+	rm $LOADER || true
 fi
 
 #if [ -f $SPINOR_LOADER ]
